bba66b00327baa40c21531069c10d611a7abeb32
Tom-ks <60391567+Tom-ks@users.noreply.github.com>
2020-03-05 13:49:52 +0100

    Basis

--- web-project/main.js
+++ web-project/main.js
@@ -1,2 +1,28 @@
-alert('Hello, JavaScript!');
-// hier wird der JavaScript-Code stehen
+const container = document.getElementById('results');
+const searchText = document.getElementById('searchText');
+
+function insertSearchResultIntoPage(jsonArray) {
+    let htmlString = '';
+    // TODO: Insert data into HTML
+    for (const jsonItem of jsonArray) {
+        htmlString += `<img src="${jsonItem.show.image.medium}"</img>`;
+    }
+    container.innerHTML = htmlString;
+}
+
+// Funktionen deklarieren.
+async function apiSearch() {
+    const searchQuery = searchText.value;
+    // JSON-Ergebnis in Konstante ablegen
+    const response = await fetch(`http://api.tvmaze.com/search/shows?q=${searchQuery}`);
+    console.log(response);
+    const json = await response.json();
+
+    // Suchergebnis in HTML-Dokument einfügen.
+    insertSearchResultIntoPage(json);
+}
+
+
+
+const btn = document.getElementById('searchButton');
+btn.addEventListener('click', apiSearch)
